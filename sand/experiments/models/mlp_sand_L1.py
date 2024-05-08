ce3b78f347f7d8362d99e269490b2c6fe5da7139
Hadi <hadi.hammoud@epfl.ch>
2024-05-08 16:32:58 +0200

    Added L1 and other methods

--- sand/experiments/models/mlp_sand_L1.py
+++ sand/experiments/models/mlp_sand_L1.py
@@ -11,7 +11,7 @@ class SAND_Layer(tf.keras.layers.Layer):
         self.sigma = sigma
         self.w = self.add_weight(
             shape=(num_inputs,),
-            initializer=tf.keras.initializers.Constant(np.ones((num_inputs,))*tf.sqrt(num_inputs_to_select/num_inputs)),
+            initializer=tf.keras.initializers.Constant(np.ones((num_inputs,))*num_inputs_to_select/num_inputs),
             trainable=True,
         )
         self.batch_norm = False
@@ -19,31 +19,14 @@ class SAND_Layer(tf.keras.layers.Layer):
     def call(self, inputs, training=False):
         wabs = tf.abs(self.w)
         wabs = wabs - tf.nn.relu(wabs - 1)
-        
-        wn = wabs / tf.sqrt(tf.reduce_sum(wabs**2)) * tf.sqrt(self.num_inputs_to_select)
+        wn = wabs / tf.reduce_sum(wabs) * self.num_inputs_to_select
         self.w.assign(wn)
-        
         inpwn = inputs * tf.expand_dims(wn, 0)
-        
         if training:
           noise = tf.random.normal(shape=tf.shape(inpwn), stddev=self.sigma * tf.abs(1-tf.expand_dims(wn, 0)))
           return inpwn + noise
         else:
           return inpwn
-        
-    def zero_out_minor_gains(self):
-        # keep the top feat_num gains and set the rest to 0
-        _, top_indices = tf.math.top_k(tf.abs(self.w), self.num_inputs_to_select)
-        mask = tf.scatter_nd(tf.reshape(top_indices, (-1, 1)), tf.ones_like(top_indices, dtype=self.w.dtype), self.w.shape)
-        self.w.assign(self.w * mask)
-
-    def binarize_gains(self):
-        # set the top feat_num gains to 1 and the rest to 0
-        _, top_indices = tf.math.top_k(tf.abs(self.w), self.num_inputs_to_select)
-        binarized_gains = tf.zeros(self.w.shape)
-        updates = tf.ones_like(top_indices, dtype=binarized_gains.dtype)
-        binarized_gains = tf.tensor_scatter_nd_update(binarized_gains, tf.reshape(top_indices, (-1, 1)), updates)
-        self.w.assign(binarized_gains)
 
 
 class SANDModel(MLPModel):
@@ -63,4 +46,5 @@ class SANDModel(MLPModel):
     return prediction
   
   def selected_indices(self):
-      return tf.argsort(tf.abs(self.select.w), direction='DESCENDING')[:self.select.num_inputs_to_select]
+      n = np.sqrt(np.sum(self.get_weights()[0]**2,axis=1))
+      return tf.argsort(tf.abs(self.select.w * n), direction='DESCENDING')[:self.select.num_inputs_to_select]
